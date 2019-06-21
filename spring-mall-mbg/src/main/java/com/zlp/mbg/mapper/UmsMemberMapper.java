5d23a74fc9cabbbd3d4008bc0a1112b540294ffb
Ygritte <ygritte.zhu@3ti.us>
2019-06-21 15:42:31 +0800

    mybatis生成

---
+++ spring-mall-mbg/src/main/java/com/zlp/mbg/mapper/UmsMemberMapper.java
@@ -0,0 +1,30 @@
+package com.zlp.mbg.mapper;
+
+import com.zlp.mbg.model.UmsMember;
+import com.zlp.mbg.model.UmsMemberExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UmsMemberMapper {
+    int countByExample(UmsMemberExample example);
+
+    int deleteByExample(UmsMemberExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UmsMember record);
+
+    int insertSelective(UmsMember record);
+
+    List<UmsMember> selectByExample(UmsMemberExample example);
+
+    UmsMember selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") UmsMember record, @Param("example") UmsMemberExample example);
+
+    int updateByExample(@Param("record") UmsMember record, @Param("example") UmsMemberExample example);
+
+    int updateByPrimaryKeySelective(UmsMember record);
+
+    int updateByPrimaryKey(UmsMember record);
+}
